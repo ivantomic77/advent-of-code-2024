6002574b8f2b8eb04f14c3672c43b8c4b636f2fa
ivan77tomic <ivan.tomic2108@gmail.com>
2024-12-02 19:11:04 +0100

    feat: Resolved AOC 2024 Day 2 Part 1

---
+++ 2/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// Bruteforced PT.1. TODO: Refactor
+	safeLevels := loadSafeData()
+	fmt.Printf("Number of safe reports: %d", len(safeLevels))
+}
+
+func loadSafeData() [][]int {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var safeLists [][]int
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		stringListItems := strings.Split(line, " ")
+
+		var listItems []int
+		for _, str := range stringListItems {
+			num, err := strconv.Atoi(str)
+			if err != nil {
+				fmt.Println("Error converting string to integer:", err)
+				continue
+			}
+			listItems = append(listItems, num)
+		}
+
+		isListSafe, _ := isSafe(listItems)
+		if isListSafe {
+			safeLists = append(safeLists, listItems)
+		}
+	}
+
+	return safeLists
+}
+
+// func removeElement(slice []int, index int) []int {
+// 	return append(slice[:index], slice[index+1:]...)
+// }
+
+func isSafe(listItems []int) (bool, int) {
+	if len(listItems) == 2 {
+		return true, -1
+	}
+
+	direction := getDirection(listItems[0], listItems[1])
+
+	for i := range listItems {
+		if i == len(listItems)-1 {
+			break
+		}
+
+		currentDirection := getDirection(listItems[i], listItems[i+1])
+
+		if currentDirection != direction {
+			fmt.Printf("DIR: %v\n%d\n\n", listItems, i)
+			return false, i
+		}
+
+		diff := int(math.Abs(float64(listItems[i] - listItems[i+1])))
+		if diff < 1 || diff > 3 {
+			fmt.Printf("DIFF: %v\n%d\n\n", listItems, i)
+			return false, i
+		}
+	}
+
+	return true, -1
+}
+
+func getDirection(a, b int) int {
+	if a > b {
+		return -1
+	}
+	return 1
+}
